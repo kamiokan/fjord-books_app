35e63f13e1eb57c273c47885b12fdb5d373f4b4f
kamiokan <spherenov@gmail.com>
2022-02-15 11:10:17 +0900

    ログイン後、ログアウト後の遷移先URLを本一覧ページへ変更した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(_resource)
+    books_path
+  end
+
+  def after_sign_out_path_for(_resource)
+    books_path
+  end
 end
