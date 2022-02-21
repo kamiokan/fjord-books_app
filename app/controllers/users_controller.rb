2ed397c33cf74078967b67170b3d543046f87d01
kamiokan <spherenov@gmail.com>
2022-02-21 11:57:12 +0900

    ユーザーの一覧画面を作成した（ページネーションもok）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  def index
+    @users = User.order(:id).page(params[:page])
+  end
+
   def show
-    @user = current_user
+    set_user
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
