91858513c344ad4ea4646bc7451d0ed59a6e1fd0
kamiokan <spherenov@gmail.com>
2022-02-22 09:30:11 +0900

    開発環境でletter_opener_webを使えるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show', :as => :user
