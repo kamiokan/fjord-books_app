88b742e1a3c13b2f373377faa862818f7c2d0ec2
kamiokan <spherenov@gmail.com>
2022-02-25 10:05:39 +0900

    アップロード可能なファイルはjpg, png, gifの3種類だけに限定した

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
   has_one_attached :icon
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validate :icon_content_type, if: :was_attached?
+
+  def icon_content_type
+    extension = ['image/png', 'image/jpg', 'image/jpeg', 'image/gif']
+    errors.add(:icon, 'の拡張子が間違っています') unless icon.content_type.in?(extension)
+  end
+
+  def was_attached?
+    icon.attached?
+  end
 end
