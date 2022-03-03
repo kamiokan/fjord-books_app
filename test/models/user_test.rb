23f05fac2e22900475bb6b49c5055388b571750c
kamiokan <spherenov@gmail.com>
2022-03-03 11:08:50 +0900

    フォロー、フォロー解除のメソッドを実装した

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should follow and unfollow a user' do
+    kamiokan = users(:kamiokan)
+    hoge = users(:hoge)
+    assert_not kamiokan.following?(hoge)
+    kamiokan.follow(hoge)
+    assert kamiokan.following?(hoge)
+    assert hoge.followers.include?(kamiokan)
+    kamiokan.unfollow(hoge)
+    assert_not kamiokan.following?(hoge)
+  end
 end
