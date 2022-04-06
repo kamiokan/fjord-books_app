5844691394bc586ce8e6a3e654626945eab5767d
kamiokan <spherenov@gmail.com>
2022-04-06 12:03:03 +0900

    usersへのルーティングをresources onlyに書き換えた

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show', :as => :user
+  resources :users, only: [:index, :show]
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
