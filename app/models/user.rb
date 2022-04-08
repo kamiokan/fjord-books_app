93093facc6d5fd53e9a5aae8bb2aa29a0c7d7cee
kamiokan <spherenov@gmail.com>
2022-04-08 14:23:56 +0900

    Userのvalidateに使うメソッドをprivateにし、メソッド名を修正した

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validate :icon_content_type, if: :was_attached?
+  validate :icon_content_type, if: :icon?
+
+  private
 
   def icon_content_type
     content_types = ['image/png', 'image/jpg', 'image/jpeg', 'image/gif']
     errors.add(:icon, 'の拡張子が間違っています') unless icon.content_type.in?(content_types)
   end
 
-  def was_attached?
+  def icon?
     icon.attached?
   end
 end
