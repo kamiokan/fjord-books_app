e6c5bbf95b2a52b4ab861559b3b68b0f73b3b960
kamiokan <spherenov@gmail.com>
2022-04-08 13:39:56 +0900

    変数名をわかりやすくcontent_typesに修正した

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   validate :icon_content_type, if: :was_attached?
 
   def icon_content_type
-    extension = ['image/png', 'image/jpg', 'image/jpeg', 'image/gif']
-    errors.add(:icon, 'の拡張子が間違っています') unless icon.content_type.in?(extension)
+    content_types = ['image/png', 'image/jpg', 'image/jpeg', 'image/gif']
+    errors.add(:icon, 'の拡張子が間違っています') unless icon.content_type.in?(content_types)
   end
 
   def was_attached?
